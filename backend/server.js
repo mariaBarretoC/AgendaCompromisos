2d45dc2afc402c4d926e0420039b53a39edb92b3
María José Barreto Castro <mbarreto@dominio.local>
2026-03-09 08:38:55 -0500

    prueba

--- backend/server.js
+++ backend/server.js
@@ -49,12 +49,16 @@ cloudinary.config({
  * - tu frontend publicado en Cloudflare Pages
  * - pruebas locales en Live Server / localhost
  */
-const allowedOrigins = [
-  "hhttps://agendacompromisos.pages.dev/",
-  "http://localhost:5500",
-  "http://127.0.0.1:5500",
-  "http://localhost:3000",
-];
+//const allowedOrigins = [
+  //"hhttps://agendacompromisos.pages.dev/",
+  //"http://localhost:5500",
+  //"http://127.0.0.1:5500",
+  //"http://localhost:3000",
+//];
+
+app.use(cors({
+  origin: "*"
+}));
 
 /**
  * Middleware CORS:
